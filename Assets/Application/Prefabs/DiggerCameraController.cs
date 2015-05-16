4f1b6ba68a30c81964d48a019c79cfe50c0e5ef5
大町隆 <takashi.omachi@happyelements.co.jp>
2015-05-16 22:05:19 +0900

    CameraController追加

---
+++ Assets/Application/Prefabs/DiggerCameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TB.Battles
+{
+    public class DiggerCameraController : MonoBehaviour
+    {
+        const float Offset = 1f;
+
+        void Start()
+        {
+            if (GameData.Instance.playerType != PlayerType.Digger)
+            {
+                enabled = false;
+            }
+        }
+
+        void LateUpdate()
+        {
+            float y = Bomber.Instance.transform.position.y + Offset;
+            if (transform.position.y > y)
+            {
+                transform.position = new Vector3(transform.position.x, y, transform.position.z);
+            }
+        }
+    }
+}
---
+++ Assets/Application/Prefabs/TetrisCameraController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TB.Battles
+{
+    public class TetrisCameraController : MonoBehaviour
+    {
+        void Start()
+        {
+            if (GameData.Instance.playerType != PlayerType.Tetris)
+            {
+                enabled = false;
+            }
+        }
+    }
+}
