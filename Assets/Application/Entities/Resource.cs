1d3d9397844f7a2c94700ed2ad64b289fea118c3
大町隆 <takashi.omachi@happyelements.co.jp>
2015-05-16 18:58:57 +0900

    Resourceクラス作成

---
+++ Assets/Application/Entities/Resource.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TB
+{
+    public class Resource : ScriptableObject
+    {
+        static Resource _instance;
+        static public Resource Instance
+        {
+            get
+            {
+                return _instance ?? (_instance = Resources.Load<Resource>("Resource"));
+            }
+        }
+    }
+}
