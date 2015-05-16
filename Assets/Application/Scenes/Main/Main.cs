b381ab343094d0ee25f24e3f7c8c07ee8197ca61
大町隆 <takashi.omachi@happyelements.co.jp>
2015-05-16 16:36:00 +0900

    Main, Matching, Titleシーン作成

---
+++ Assets/Application/Scenes/Main/Main.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TB.Mains
+{
+    public class Main : MonoBehaviour
+    {
+        void Start()
+        {
+            Application.LoadLevel("Title");
+        }
+    }
+}
---
+++ Assets/Application/Scenes/Title/Title.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace TB.Titles
+{
+    public class Title : MonoBehaviour
+    {
+
+    }
+}
