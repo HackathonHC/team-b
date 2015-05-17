01ae4f6ec954ecd7345c6f58509ea8f2549b7074
大町隆 <takashi.omachi@happyelements.co.jp>
2015-05-17 13:20:06 +0900

    テトリス側はドリル側端末のブロック生成処理を待つよう修正

--- Assets/Application/Prefabs/TetrisCameraController.cs
+++ Assets/Application/Prefabs/TetrisCameraController.cs
@@ -17,7 +17,8 @@ namespace TB.Battles
 
         void LateUpdate()
         {
-            if(TetrisPlayer.Instance.TetrisBlock != null)
+            var player = TetrisPlayer.Instance;
+            if(player && player.TetrisBlock != null)
             {
                 var position = TetrisPlayer.Instance.TetrisBlock.transform.position + new Vector3(0, Offset, 0);
                 transform.localPosition = new Vector3(transform.localPosition.x, position.y, transform.localPosition.z);
